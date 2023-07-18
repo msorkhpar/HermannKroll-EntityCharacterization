d93462a3a3d5917115230078dca44e70f74bfef3
Mo Sorkhpar <Sorkhpar@outlook.com>
2023-07-18 11:42:39 -0400

    - Add libs and dataset to the forked project ( including results)

---
+++ jar-installer.sh
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=libs/jawjaw-1.0.2.jar -DgroupId=jawjaw -DartifactId=jawjaw -Dversion=1.0.2 -Dpackaging=jar
+mvn install:install-file -Dfile=libs/ws4j-1.0.1.jar -DgroupId=ws4j -DartifactId=ws4j -Dversion=1.0.1 -Dpackaging=jar
+mvn install:install-file -Dfile=libs/virtjdbc-4.2.jar -DgroupId=jdbc -DartifactId=virtjdbc -Dversion=4.2 -Dpackaging=jar
